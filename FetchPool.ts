0c71c5d09af522c2d8a64821a1e67bf15487daa4
ASchwartzman <aleschwartzman22@gmail.com>
2022-06-06 22:07:13 -0300

    Fetch pool data

---
+++ FetchPool.ts
@@ -0,0 +1,103 @@
+import { ethers } from "ethers";
+import { Pool } from "@uniswap/v3-sdk";
+import { Token } from "@uniswap/sdk-core";
+import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+
+const provider = ethers.getDefaultProvider("matic");
+const poolAddress = "0x357faF5843c7FD7fb4E34FBEabDAc16eabE8a5bc";
+const poolContract = new ethers.Contract(
+  poolAddress,
+  IUniswapV3PoolABI,
+  provider
+);
+
+interface Immutables {
+  factory: string;
+  token0: string;
+  token1: string;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: ethers.BigNumber;
+}
+
+interface State {
+  liquidity: ethers.BigNumber;
+  sqrtPriceX96: ethers.BigNumber;
+  tick: number;
+  observationIndex: number;
+  observationCardinality: number;
+  observationCardinalityNext: number;
+  feeProtocol: number;
+  unlocked: boolean;
+}
+
+async function getPoolImmutables() {
+  const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
+    await Promise.all([
+      poolContract.factory(),
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee(),
+      poolContract.tickSpacing(),
+      poolContract.maxLiquidityPerTick(),
+    ]);
+
+  const immutables: Immutables = {
+    factory,
+    token0,
+    token1,
+    fee,
+    tickSpacing,
+    maxLiquidityPerTick,
+  };
+  return immutables;
+}
+
+async function getPoolState() {
+  const [liquidity, slot] = await Promise.all([
+    poolContract.liquidity(),
+    poolContract.slot0(),
+  ]);
+
+  const PoolState: State = {
+    liquidity,
+    sqrtPriceX96: slot[0],
+    tick: slot[1],
+    observationIndex: slot[2],
+    observationCardinality: slot[3],
+    observationCardinalityNext: slot[4],
+    feeProtocol: slot[5],
+    unlocked: slot[6],
+  };
+
+  return PoolState;
+}
+
+async function main() {
+  const [immutables, state] = await Promise.all([
+    getPoolImmutables(),
+    getPoolState(),
+  ]);
+
+  const TokenA = new Token(137, immutables.token0, 18, "WETH", "Wrapped Ether");
+
+  const TokenB = new Token(
+    137,
+    immutables.token1,
+    18,
+    "UNI",
+    "Uniswap Protocol"
+  );
+
+  const poolExample = new Pool(
+    TokenA,
+    TokenB,
+    immutables.fee,
+    state.sqrtPriceX96.toString(),
+    state.liquidity.toString(),
+    state.tick
+  );
+  console.log(poolExample);
+}
+
+main();
---
+++ index_old.ts
@@ -0,0 +1,48 @@
+import { ethers } from "ethers";
+import { Address } from "cluster";
+
+const provider = ethers.getDefaultProvider("matic", {
+  etherscan: "WF6JKMSGW7I2JS4BW2PHAAKNCXAJVTA2IG",
+});
+
+const poolAddress = "0x357faF5843c7FD7fb4E34FBEabDAc16eabE8a5bc";
+
+const poolImmutablesAbi = [
+  "function factory() external view returns (address)",
+  "function token0() external view returns (address)",
+  "function token1() external view returns (address)",
+  "function fee() external view returns (uint24)",
+  "function tickSpacing() external view returns (int24)",
+  "function maxLiquidityPerTick() external view returns (uint128)",
+];
+
+const poolContract = new ethers.Contract(
+  poolAddress,
+  poolImmutablesAbi,
+  provider
+);
+
+interface Immutables {
+  factory: Address;
+  token0: Address;
+  token1: Address;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: number;
+}
+
+async function getPoolImmutables() {
+  const PoolImmutables: Immutables = {
+    factory: await poolContract.factory(),
+    token0: await poolContract.token0(),
+    token1: await poolContract.token1(),
+    fee: await poolContract.fee(),
+    tickSpacing: await poolContract.tickSpacing(),
+    maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
+  };
+  return PoolImmutables;
+}
+
+getPoolImmutables().then((result) => {
+  console.log(result);
+});
